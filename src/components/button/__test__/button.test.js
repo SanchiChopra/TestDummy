9b1c7459f96b5f7aaa8cc16101c3023586529025
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2020-12-09 20:28:08 +0530

    multiple buttons testing complete

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from './../button';
 
+import { render, cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(cleanup);
+
 it("renders without crashing", ()=> {
     const div = document.createElement("div");
     ReactDOM.render(<Button></Button>, div)
+})
+
+it("renders button correctly", ()=> {
+    const {getByTestId} = render(<Button label="click me please"> </Button>)
+    expect(getByTestId('button')).toHaveTextContent("click me please")
+})
+
+it("renders button correctly", ()=> {
+    const {getByTestId} = render(<Button label="save"> </Button>)
+    expect(getByTestId('button')).toHaveTextContent("save")
 })
